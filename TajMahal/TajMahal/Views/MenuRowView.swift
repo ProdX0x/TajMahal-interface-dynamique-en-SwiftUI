79e306bb905abab19bb8b64ad6efd9af77067cc0
ProdX0x <ss_saulnier@icloud.com>
2025-02-24 09:26:04 +0100

    Ajout et mise à jour des fichiers du projet

---
+++ TajMahal/TajMahal/Views/MenuRowView.swift
@@ -0,0 +1,18 @@
+//
+//  MenuRowView.swift
+//  TajMahal
+//
+//  Created by Stéphane SAULNIER on 23/02/2025.
+//
+
+import SwiftUI
+
+struct MenuRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuRowView()
+}
---
+++ TajMahal/TajMahal/Views/SpicerView.swift
@@ -0,0 +1,18 @@
+//
+//  SpicerView.swift
+//  TajMahal
+//
+//  Created by Stéphane SAULNIER on 23/02/2025.
+//
+
+import SwiftUI
+
+struct SpicerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SpicerView()
+}
